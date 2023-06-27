61407af832e8d993911e57c7ae2e5426d7e9159d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-27 23:51:37 +0900

    SWR 적용

--- src/app/chat/ChatClient.tsx
+++ src/app/chat/ChatClient.tsx
@@ -2,6 +2,8 @@
 import { User } from "@prisma/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
+import { TUserWithChat } from "../../../types";
 
 interface ChatClientProps {
   currentUser: User | null;
@@ -16,16 +18,26 @@ const ChatClient = ({ currentUser }: ChatClientProps) => {
   console.log(currentUser);
 
   const [layout, setLayout] = useState(false);
-  useEffect(() => {
-    axios.get("/api/chat").then((res) => console.log(res));
-  }, []);
+
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useSWR("/api/chat", fetcher, {
+    refreshInterval: 1000,
+  });
+
+  users?.find((user: TUserWithChat) => user.email === currentUser?.email);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
 
   return (
     <main>
       <div className="grid grid-cols-[1fr] md:grid-cols-[300px_1fr]">
-        Chat
-        <section className={`md:flex ${layout && "hidden"}`}>{/* Contact Component */}</section>
-        <section className={`md:flex ${!layout && "hidden"}`}>{/* Chat Component */}</section>
+        <section className={`md:flex ${layout && "hidden"}`}>Contact Component</section>
+        <section className={`md:flex ${!layout && "hidden"}`}>Chat Component</section>
       </div>
     </main>
   );
