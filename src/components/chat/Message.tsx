d40800677d1d335cf1cb7b086085447c320433fa
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 10:35:23 +0900

    Chat 사진 업로드 기능 구현

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Avatar from "../Avatar";
 import { fromNow } from "@/helpers/dayjs";
+import Image from "next/image";
 
 interface MessageProps {
   isSender: boolean;
@@ -23,6 +24,11 @@ const Message = ({ isSender, messageText, messageImage, receiverName, receiverIm
           <span className="font-medium">{isSender ? "You" : receiverName}</span>
           <span className="texxt-xs text-gray-500 opacity-80">{fromNow(time)}</span>
         </div>
+        {messageImage && (
+          <div className="overflow-hidden rounded-md mx-[0.6rem] max-w-[80%]">
+            <Image src={messageImage} width={300} height={300} alt="" />
+          </div>
+        )}
         {messageText && (
           <div className={`p-2 break-all text-white rounded-lg ${isSender ? "bg-orange-500 rounded-tr-none" : "bg-gray-400 rounded-tl-none"}`}>
             <p>{messageText}</p>
