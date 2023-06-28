c645ab5ca996ee12377c8ad7bc7af77dba5e0fa8
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 09:34:56 +0900

    ChatHeader Component 작성

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TUserWithChat } from "../../../types";
 import Input from "./Input";
+import ChatHeader from "./ChatHeader";
 
 interface ChatProps {
   currentUser: TUserWithChat;
@@ -13,13 +14,21 @@ interface ChatProps {
 }
 
 const Chat = ({ currentUser, receiver, setLayout }: ChatProps) => {
+  const conversation = currentUser?.conversations.find((conversation) => conversation.users.find((user) => user.id === receiver.receiverId));
   if (!receiver.receiverName || !currentUser) {
     return <div className="w-full h-full"></div>;
   }
   return (
     <div className="w-full">
-      <div></div>
-      <div className="flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]"></div>
+      <div>
+        <ChatHeader
+          setLayout={setLayout}
+          receiverName={receiver.receiverName}
+          receiverImage={receiver.receiverImage}
+          lastMessageTime={conversation?.messages.filter((message) => message.receiverId === currentUser.id).slice(-1)[0]?.createdAt}
+        />
+      </div>
+      <div className="flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]">Chat Message</div>
       <div>
         <Input receiverId={receiver?.receiverId} currentUserId={currentUser?.id} />
       </div>
